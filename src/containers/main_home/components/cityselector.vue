60674278d9db6bafc7e90fef74965b47609e48e0
Francois Rice <francoisrice@gmail.com>
2018-01-19 15:02:24 -0500

    Improved City Selector

--- src/containers/main_home/components/cityselector.vue
+++ src/containers/main_home/components/cityselector.vue
@@ -3,14 +3,24 @@
     <div class="row">
       <div class="col-lg-12">
         City Selector
+        <v-select :options="cities"/>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import vSelect from 'vue-select'
 
 export default {
-  props: ['collection']
+  props: ['collection'],
+  components: {
+    vSelect
+  },
+  computed: {
+    cities () {
+      return this.$store.getters['record/cities']
+    }
+  }
 }
 </script>
