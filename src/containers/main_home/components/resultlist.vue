6cb7294dbd3a84623346040360943f858c9c57a5
Francois Rice <francoisrice@gmail.com>
2018-01-19 14:50:30 -0500

    Basic Layout

---
+++ src/containers/main_home/components/resultlist.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="card card-body">
+    <div class="row">
+      <div class="col-lg-12">
+        Result List
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: ['collection']
+}
+</script>
--- src/containers/main_home/index.vue
+++ src/containers/main_home/index.vue
@@ -1,6 +1,6 @@
 
 <template>
-  <div class="container-fluid">
+  <div class="container-fluid pt-4">
     <i class="fa fa-spin fa-spinner" v-if="fetching"/>
     <LayoutView :collection="collection" v-if="!fetching"/>
   </div>
